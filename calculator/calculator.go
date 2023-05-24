8fae91a51c3c29f94da4d07e3feeb9409d9abf37
ian.jungmann <ian.jungmann@piecestech.com>
2023-05-24 00:12:51 -0500

    Adding a basic calculator

---
+++ calculator/calculator.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Operation struct {
+	operation string
+	first     int
+	second    int
+	result    int
+}
+
+func main() {
+	fmt.Println("Enter an integer")
+	reader := bufio.NewScanner(os.Stdin)
+	reader.Scan()
+	result, _ := strconv.Atoi(reader.Text())
+OperandLoop:
+	for {
+		fmt.Println("Enter an operand")
+		reader.Scan()
+		operand := reader.Text()
+		fmt.Println("Enter an integer")
+		reader.Scan()
+		affector, _ := strconv.Atoi(reader.Text())
+
+		switch operand {
+		case "+":
+			fmt.Println("Adding")
+			result += affector
+		case "-":
+			fmt.Println("Subtracting")
+			result -= affector
+		case "*":
+			fmt.Println("Multiplying")
+			result *= affector
+		case "/":
+			fmt.Println("Dividing")
+			result /= affector
+		default:
+			fmt.Println("Breaking")
+			break OperandLoop
+		}
+		fmt.Printf("result is %d\n", result)
+	}
+}
